6bfcd9e65709883f7bfc536b927f2a709fb69069
sNowJami <3493279939@qq.com>
2026-06-16 18:03:40 +0800

    SwithButton + StartText

--- Source/Hide/Private/Interaction/InteractionActorBase.cpp
+++ Source/Hide/Private/Interaction/InteractionActorBase.cpp
@@ -7,16 +7,21 @@
 AInteractionActorBase::AInteractionActorBase()
 {
     PrimaryActorTick.bCanEverTick = true;
+    USceneComponent* DummyRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DummyRoot"));
+    RootComponent = DummyRoot;
     ActorCollision = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
-    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
-    MeshComponent->SetupAttachment(ActorCollision);
-    RootComponent = ActorCollision;
+    ActorCollision->SetupAttachment(RootComponent); 
     ActorCollision->SetSphereRadius(100.f);
+
+    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+    MeshComponent->SetupAttachment(RootComponent); 
     MeshComponent->SetRelativeLocation(FVector::ZeroVector);
 
     MeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
     MeshComponent->SetCollisionObjectType(ECC_WorldDynamic);
-    MeshComponent->SetCollisionResponseToAllChannels(ECR_Overlap);
+    MeshComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+    MeshComponent->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+    MeshComponent->SetCollisionResponseToChannel(ECC_Camera, ECR_Block);
 }
 
 void AInteractionActorBase::BeginPlay()
